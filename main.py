10daefe7b57d2f4e8d9de70505c67163dd4aa448
Julio Gtz <jc.gutierrezq@gmail.com>
2021-11-09 10:52:28 -0700

    validations_body

--- main.py
+++ main.py
@@ -6,12 +6,17 @@ from pydantic import BaseModel
 
 #FastAPI
 from fastapi import FastAPI
-from fastapi import Body, Query
+from fastapi import Body, Query, Path
 
 app = FastAPI()
 
 
 # Models
+
+class Location(BaseModel):
+	city: str
+	state: str
+	country: str
 
 class Person(BaseModel):
 	frist_name: str
@@ -19,6 +24,8 @@ class Person(BaseModel):
 	age: int 
 	hair_color: Optional[str] = None
 	is_married: Optional[bool] = None
+
+
 
 
 
@@ -40,6 +47,45 @@ def create_person(person: Person = Body(...)):
 @app.get("/person/detail")
 def show_person(
 	name: Optional[str] = Query(None, min_length=1, max_length=50),
-	age: str = Query(...)
+	title="Person Name", 
+	description="This is the person name. It's between 1 and 50 characters", 
+	age: str = Query(
+		...,
+		title="Person Age",
+		description="This is the person age. It's required"
+		)
 ):
 	return {name: age}
+
+
+# Validacfiones: Path Parameters
+
+@app.get("/person/detail/{person_id}")
+def show_person(
+	person_id: int = Path(
+		..., 
+		gt=0,
+		title="Id Person",
+		description="This is the id of the person"
+		)
+):
+	return {person_id: "It exists:!"}
+
+
+# Validaciones: Request body
+
+@app.put("/person/{person_id}")
+def update_person(
+	person_id: int = Path(
+		...,
+		title="Person Id",
+		description="This is the person ID",
+		gt=0
+	),
+	person: Person = Body(...),
+	location: Location = Body(...)
+
+):
+	results = person.dict()
+	results.update(location.dict())
+	return results
